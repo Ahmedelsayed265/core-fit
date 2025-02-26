aba770ca2d920eb7fb3bd5f0ab10a1c084ea379f
Abdelrahman-Osama0 <https://github.com/Abdelrahman-Osama0>
2025-02-26 23:22:33 +0200

    test deploy (change CustomAuthenticationEntryPoint in security config)

--- src/main/java/com/corefit/config/SecurityConfig.java
+++ src/main/java/com/corefit/config/SecurityConfig.java
@@ -72,8 +72,8 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("*"));
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
-        configuration.setAllowedOrigins(List.of("https://core-fit-production.up.railway.app"));
+       // configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+       // configuration.setAllowedOrigins(List.of("https://core-fit-production.up.railway.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
